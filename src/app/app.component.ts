c2481a29fa2ff5b697178b5edf13dca9b3e5268d
dobleviatech@gmail.com <tecnologia@doblevia.org>
2024-07-02 18:27:47 -0500

    Bases Angular 01

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,25 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+
 })
 export class AppComponent {
-  title = 'bases';
+  public title: string = 'hola';
+  public counter:number=10;
+
+  // increaseBy():void{
+  //   this.counter +=1;
+  // }
+
+  //asi serviria para la suma y resta
+  // increaseBy(value: number):void{
+  //   this.counter +=value;
+  // }
+
+  // resetCounter(){
+  //   this.counter =10;
+  // }
+
+
+
 }
---
+++ src/app/heroes/hero/hero.component.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes-hero',
+  templateUrl: './hero.component.html',
+  styleUrl: './hero.component.css'
+})
+export class HeroComponent {
+
+
+public name: string = 'ironman';
+public age: number = 45;
+
+//Getter
+get capitalizedName():string {
+  //return 'Hola mundo'.toUpperCase();
+  return this.name.toUpperCase();
+
+}
+
+//metodo
+
+getHeroDescription():string {
+
+  return `${this.name} - ${this.age}`;
+}
+
+changeHero():void{
+this.name = 'Spiderman';
+
+
+}
+
+changeAge(){
+this.age = 25;
+
+}
+
+resetForm(): void{
+  this.name='ironman';
+  this.age=45;
+}
+
+
+
+}
